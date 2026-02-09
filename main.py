9fc68db9670eeba94ae41954b9928c11c68d0928
m92494303-ops <m92494303@gmail.com>
2026-02-09 20:51:33 +0500

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from telebot import TeleBot
 import os
 
-from handlers.start import start_handler
-from handlers.ram import ram_handler
-from handlers.admin import admin_handler
-from handlers.premium import premium_handler
+from start import start_handler
+from ram import ram_handler
+from admin import admin_handler
+from premium import premium_handler
 
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
 bot = TeleBot(TOKEN)
 
-# handlerlarni ulaymiz
 start_handler(bot)
 ram_handler(bot)
 admin_handler(bot)
 premium_handler(bot)
 
-# botni ishga tushiramiz
 bot.polling(none_stop=True)
